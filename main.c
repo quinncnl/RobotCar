01280d08fe89c33e2056f182fee705a0b293b4eb
Tsai Tsing <clear.tsai@gmail.com>
2014-03-21 16:57:30 +0100

    using corner detection

--- main.c
+++ main.c
@@ -3,46 +3,20 @@
 #include  "adc.h"
 
 struct map {
-	int posDis;
 	int posTurn;
-	int *arrDis;
 	int *arrTurn;
 };
-
 
 // -1: right
 // 1: left
 // 0: nothing
 // 2: turn around
 
-
-
 struct map path;
 
-int arrDisToBank[10] = {100, 100, 100, 100};
-int arrTurnToBank[10] = {0, 2, 1, -1};
-void setPathToBank(){
-	path.posDis = 3;
-	path.posTurn = 3;
-	path.arrDis = arrDisToBank;
-	path.arrTurn = arrTurnToBank;
-}
-
-int arrDisToRiver[10] = {100, 100, 100, 100, 40, 30, 50, 20};
-int arrTurnToRiver[10] = {0, 2, 1, -1, 2, 1, -1, 2};
-void setPathToRiver(){
-	path.posDis = 7;
-	path.posTurn = 7;
-	path.arrDis = arrDisToRiver;
-	path.arrTurn = arrTurnToRiver;
-}
-
-int arrDisToTest[10] = {40, 50, 40};
 int arrTurnToTest[10] = {2, 1, 1};
 void setPathToTest(){
-	path.posDis = 2;
 	path.posTurn = 2;
-	path.arrDis = arrDisToTest;
 	path.arrTurn = arrTurnToTest;
 }
 
@@ -51,18 +25,23 @@ void forwardLeftWheel() { P2OUT &=~ BIT1; }
 void backwardLeftWheel() { P2OUT |= BIT1; }
 void forwardRightWheel(){ P2OUT &=~ BIT5; }
 void backwardRightWheel(){ P2OUT |= BIT5; }
+void forwardWheel(){ forwardLeftWheel(); forwardRightWheel(); }
+void backwardWheel(){ backwardLeftWheel(); backwardRightWheel(); }
 
-void startLeftWheel(){ TA1CCR1 = 850; }
+
+void startLeftWheel(){ TA1CCR1 = 900; }
 void stopLeftWheel(){ TA1CCR1 = 0; }
-void startRightWheel(){ TA1CCR2 = 800; }
+void startRightWheel(){ TA1CCR2 = 900; }
 void stopRightWheel(){ TA1CCR2 = 0; }
-void slowLeftWheel(){ TA1CCR1 = 550; }
-void slowRightWheel(){ TA1CCR2 = 520; }
+void slowLeftWheel(){ TA1CCR1 = 100; }
+void slowRightWheel(){ TA1CCR2 = 250; }
+void slow() { slowRightWheel(); slowLeftWheel(); }
+void stop(){ stopLeftWheel(); stopRightWheel(); }
 
 void turnLeft(){
-	backwardLeftWheel();
+	//backwardLeftWheel();
 	forwardRightWheel();
-	startLeftWheel();
+	slowLeftWheel();
 	startRightWheel();
 
 	long i = 37000;
@@ -86,54 +65,36 @@ void turnRight(){
 // 0: straight
 // 1: other
 int onStraigt = 0;
-int distance = 0;
 void turnAround(){ turnLeft();turnLeft(); }
 
-void goStaight(int dis){
+void goStaight(){
 	onStraigt = 1;
-	distance = dis;
 	startLeftWheel();
 	startRightWheel();
-	path.posDis--;
 }
 void goStaightIterate(){
-	distance--;
-	if (distance == 0) {
-		stopLeftWheel();
-		stopRightWheel();
-		onStraigt = 0;
 
-		switch (path.arrTurn[path.posTurn]) {
-		case 1:
-			turnLeft();
-	        break;
-		case -1:
-			turnRight();
-			break;
-		case 2:
-			turnAround();
-			break;
-		}
+	onStraigt = 0;
 
-		if (path.posDis < 0) {
-			stopLeftWheel();
-			stopRightWheel();
-			exit(0);
-		}
-
-		path.posTurn--;
-		goStaight(path.arrDis[path.posDis]);
- 
+	switch (path.arrTurn[path.posTurn]) {
+	case 1:
+		turnLeft();
+		break;
+	case -1:
+		turnRight();
+		break;
+	case 2:
+		turnAround();
+		break;
 	}
+
+	path.posTurn--;
 }
 void route(){
-	if (onStraigt)
-		goStaightIterate();
 }
 
 void routeToBank(){
 	setPathToTest();
-	goStaight(path.arrDis[path.posDis]);
 }
 
 
@@ -153,7 +114,7 @@ void initDistanceSensor(){
 __interrupt void Port_2(void)
 {
 	route();
-//	P1OUT ^= BIT6;
+	//	P1OUT ^= BIT6;
 	P2IFG &=~ (BIT0);
 }
 
@@ -176,6 +137,8 @@ void offLED0(){	P1OUT &=~ BIT0; }
 void onLED6(){ P1OUT |= BIT6; }
 void offLED6(){	P1OUT &=~ BIT6; }
 
+int routeMissCounter = 0;
+int leftSensor, rightSensor;
 void main(void)
 {
 	WDTCTL = WDTPW + WDTHOLD;
@@ -200,15 +163,17 @@ void main(void)
 	  forwardLeftWheel();
 	  goStaight(100);
 	*/
-	routeToBank();
+	// routeToBank();
 
+	goStaight();
+	forwardWheel();
 	while(1){
 
 		front_sensor = read_adc(4);
 		left_line_sensor = read_adc(1);
 		right_line_sensor = read_adc(2);
+		/*
 		if (front_sensor > 400) {
-			// stop
 			stopRightWheel();
 			stopLeftWheel();
 		}
@@ -219,24 +184,52 @@ void main(void)
 			startLeftWheel();
 			startRightWheel();
 		}
+		*/
 
-		if (left_line_sensor > 700) {
+		if (left_line_sensor > 600) {
 			// not touching line
-			startLeftWheel();
+			leftSensor = 1;
 			onLED0();
 		}
 		else {
-			slowLeftWheel();
+			leftSensor = 0;
 			offLED0();
 		}
 
-		if (right_line_sensor > 700) {
-			startRightWheel();
+		if (right_line_sensor > 600) {
+			rightSensor = 1;
 			onLED6();
 		}
 		else {
-			slowRightWheel();
+			rightSensor = 0;
 			offLED6();
 		}
+
+		if (leftSensor && !rightSensor) {
+			// right touching
+			//startLeftWheel();
+			forwardLeftWheel();
+			backwardRightWheel();
+
+		}
+		if (!leftSensor && rightSensor) {
+			// left touching
+			//startRightWheel();
+			forwardRightWheel();
+			backwardLeftWheel();
+		}
+		if (leftSensor && rightSensor) {
+			// not touching
+			forwardWheel();
+			slow();
+		}
+
+		// Touching both line means it meets a turn.
+		if (!leftSensor && !rightSensor) {
+			//stop();
+			turnLeft();
+		}
+
+	
 	}
 }
